b4109c0ea07080936d45b2386d203baaceb9ec9d
hata0 <haataa0100@gmail.com>
2025-10-17 14:51:07 +0900

    feat: ナビゲーション機能を導入

--- app/src/main/java/com/hata/travelapp/MainActivity.kt
+++ app/src/main/java/com/hata/travelapp/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.hata.travelapp.screen.trip.TripScreen
+import com.hata.travelapp.internal.presentation.android.trip.view.TripScreen
 import com.hata.travelapp.ui.theme.TravelAppTheme
 
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/hata/travelapp/internal/presentation/android/trip/view/TripScreen.kt
+++ app/src/main/java/com/hata/travelapp/internal/presentation/android/trip/view/TripScreen.kt
@@ -1,4 +1,4 @@
-package com.hata.travelapp.screen.trip
+package com.hata.travelapp.internal.presentation.android.trip.view
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
