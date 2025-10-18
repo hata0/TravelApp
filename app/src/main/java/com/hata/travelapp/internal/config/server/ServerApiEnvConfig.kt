632e9226b7fa8ffa369fe801a0b3e07abf319e01
hata0 <haataa0100@gmail.com>
2025-10-18 16:22:18 +0900

    refactor: configのロジック改善

--- app/src/main/java/com/hata/travelapp/internal/config/server/ServerApiEnvConfig.kt
+++ app/src/main/java/com/hata/travelapp/internal/config/server/ServerApiEnvConfig.kt
@@ -2,14 +2,6 @@ package com.hata.travelapp.internal.config.server
 
 import com.hata.travelapp.BuildConfig
 
-class ServerApiEnvConfig private constructor(
-    override val baseUrl: String
-): ServerApiConfig{
-    companion object {
-        fun load(): ServerApiEnvConfig {
-            return ServerApiEnvConfig(
-                BuildConfig.SERVER_API_BASE_URL
-            )
-        }
-    }
-}
+data class ServerApiEnvConfig(
+    override val baseUrl: String = BuildConfig.SERVER_API_BASE_URL
+) : ServerApiConfig
